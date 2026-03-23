5043c1e12c8dadbf06731e85e1ae28aa2899798e
samricodev <samuel.isaac.rico.estrada@gmail.com>
2026-03-23 12:05:59 -0600

    feat: update greeting message and add user module files

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello Fucking World!"', () => {
+      expect(appController.getHello()).toBe('Hello Fucking World!');
     });
   });
 });
